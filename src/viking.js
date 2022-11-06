7cd20bb898ef5b0e1b00317cb7ef4d844fc30859
yuyujulz <ganina.julia2@gmail.com>
2022-11-06 20:04:29 +0000

    forgot to push, not fully done

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,38 @@
 // Soldier
-class Soldier {}
-
+class Soldier {
+    constructor(health, strength){
+        this.health = health
+        this.strength = strength
+    }
+    attack(){
+        return this.strength
+    }
+    receiveDamage(damage){
+        
+        damage = this.health - this.strength
+        
+}
+}
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(name, health, strength)
+        this.name = name
+        this.health = health
+        this.strength = strength
+    }
+    receiveDamage(damage){
+        damage = this.health - this.strength}
+        
+    battleCry(){ 
+        if (this.health > 0){
+        return `${this.name} has received ${damage} points of damage`
+        } else {
+        return `${this.name} has died in act of combat`
+    }}
+    
+
+}
 
 // Saxon
 class Saxon {}
